75a94041071a2af2f57975d721756ce369c7210d
Junki Kikuchi <jkikuchi00@gmail.com>
2019-05-17 01:02:50 +0900

    Change linked_files

--- config/deploy.rb
+++ config/deploy.rb
@@ -45,7 +45,7 @@ set :default_env, {
 # set :ssh_options, verify_host_key: :secure
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
-set :linked_files, %w{ config/master.key }
+set :linked_files, %w{ config/master.key config/credentials.yml.enc }
 
 set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1'

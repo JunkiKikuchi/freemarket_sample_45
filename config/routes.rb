5773b2582e09b5f27bcf11ec1df08775de9c3107
taketakedesu <ketaketadesu@gmail.com>
2019-05-26 19:58:46 +0900

    ログアウト　ルーティング設定
    ログアウトボタンへのルーティングを設定しました

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'products#index'
   get 'products' => 'products#index'
-end
+  get 'logout' => 'logout#index'
+ end

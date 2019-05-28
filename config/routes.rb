d679fdde1023f9f949829e52eb715dd75ad34b5b
taketakedesu <ketaketadesu@gmail.com>
2019-05-28 23:31:06 +0900

    route編集
    ハッシュロケットを消しました。

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'products#index'
-  get 'products' => 'products#index'
-  get 'registrations' => 'registrations#index'
-  get 'phones' => 'phones#index'
-  get 'address' => 'address_confirmation#index'
-  get 'payment' => 'payment_confirmation#index'
-  get 'registration_finish' => 'registration_finish#index'
+  get products: :"products#index"
+  get registrations: :'registrations#index'
+  get phones: :'phones#index'
+  get address: :'address_confirmation#index'
+  get payment: :'payment_confirmation#index'
+  get registration_finish: :'registration_finish#index'
 end

600da3ce55384b6b66420ffcd401100afeb9823c
taketakedesu <ketaketadesu@gmail.com>
2019-06-04 00:45:34 +0900

    ルーティング設定
    クレジットカード追加のルーティングを設定いたしました

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'products#index'
   get 'products' => 'products#index'
+
+  get 'card_add' ,to: 'card_add#index'
 end

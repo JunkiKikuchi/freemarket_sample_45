76211d69eb4ca6171f80e6d785a9c36b55494139
taketakedesu <ketaketadesu@gmail.com>
2019-06-04 10:25:32 +0900

    cnflict解消
    コンフリクトを解消しました

--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'products#index'
-<<<<<<< HEAD
-  get 'products' => 'products#index'
 
   get 'card_add' ,to: 'card_add#index'
-=======
   get 'registrations' ,to: 'registrations#index'
   get 'phones' ,to: 'phones#index'
   get 'address' ,to: 'address_confirmation#index'
   get 'payment' ,to: 'payment_confirmation#index'
   get 'registration_finish' ,to: 'registration_finish#index'
->>>>>>> JunkiKikuchi/master
 end

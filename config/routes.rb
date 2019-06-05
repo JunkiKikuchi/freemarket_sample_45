6b4807538307194c2c2a1f7cd4b582aa648b70ea
taketakedesu <ketaketadesu@gmail.com>
2019-06-06 01:39:01 +0900

    conflict修正
    コンフリクト している記述を削除しました

--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'products#index'
-<<<<<<< HEAD
-  get 'products' => 'products#index'
   get 'logout' => 'logout#index'
- end
-=======
   get 'registrations' ,to: 'registrations#index'
   get 'phones' ,to: 'phones#index'
   get 'address' ,to: 'address_confirmation#index'
   get 'payment' ,to: 'payment_confirmation#index'
   get 'registration_finish' ,to: 'registration_finish#index'
 end
->>>>>>> JunkiKikuchi/master

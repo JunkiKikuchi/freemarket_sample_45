932f3a0cb17c843fdd5d1c835770b3d159b472fd
Keiko <kco.kalavinka1207@gmail.com>
2019-06-08 16:36:49 +0900

    My page 修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'products#index'
   get 'products' => 'products#index'
-  get 'mypages' => 'mypages#index'
+  get 'mypage' => 'mypages#index'
+  get 'profile' => 'mypages#profile'
 end

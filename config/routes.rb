e80cd98c82d6e9231ee28e78f3724ccefcae6e9c
taketakedesu <ketaketadesu@gmail.com>
2019-06-20 20:04:56 +0900

    ハッシュロケットを削除しました

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'products#index'
-  get 'logout' => 'logout#index'
+  get 'logout' ,to: 'logout#index'
   get 'registrations' ,to: 'registrations#index'
   get 'phones' ,to: 'phones#index'
   get 'address' ,to: 'address_confirmation#index'
